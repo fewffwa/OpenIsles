2a87e5a371cdee1ebbf258b24c10d9b33dd6ef7a
Jake Jefferson <yourface255@yahoo.com>
2026-09-01 15:28:57 -0700

    initial mod

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "modding.h"
+#include "ultra64.h"
+#include "enums.h"
+#include "common_structs.h"
+
+extern Maps current_map;
+void setFlag(s16 flagIndex, u8 newValue, u8 flagType);
+
+RECOMP_CALLBACK("*", dk64recomp_every_frame) void unlock_extras_menu(void) {
+    for (int i=0x1BB; i<=0x1C3; i++){
+    setFlag(i, TRUE, FLAG_TYPE_PERM);
+    }
+    for (int i=0x1CD; i<=0x1D4; i++){
+    setFlag(i, TRUE, FLAG_TYPE_PERM);
+    }
+    for (int i=0x182; i<=0x185; i++){
+    setFlag(i, TRUE, FLAG_TYPE_PERM);
+    }
+        setFlag(0x6, TRUE, FLAG_TYPE_PERM);
+        setFlag(0x42, TRUE, FLAG_TYPE_PERM);
+        setFlag(0x46, TRUE, FLAG_TYPE_PERM);
+        setFlag(0x75, TRUE, FLAG_TYPE_PERM);
+        setFlag(0x179, TRUE, FLAG_TYPE_PERM);
+}
